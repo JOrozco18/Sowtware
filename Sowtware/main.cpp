a638be4bdb34c7572f2d1e1334acf2cb28c7c87f
Julian Orozco <JulianOrozco@Julians-MacBook-Pro.local>
2014-11-05 21:18:56 -0600

    Initial Commit

---
+++ Sowtware/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sowtware
+//
+//  Created by Julian Orozco on 11/5/14.
+//  Copyright (c) 2014 Julian Orozco. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
